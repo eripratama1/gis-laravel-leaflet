690a2db9c29528ac51e0f55c165b336e9d101197
Eri Pratama <eripratama90@gmail.com>
2023-03-25 17:12:45 +0800

    setup new cdn and token

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -11,24 +11,21 @@
 
     {{-- Pada view map.blade ini kita menambahkan beberapa cdn diantaranya
     bootstrap, leaflet css dan js, leaflet control search css dan js --}}
-    
+
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
-        integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
-        crossorigin="" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.1/dist/leaflet.css"
+        integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin="" />
 
-    <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
-        integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
-        crossorigin="">
-    </script>
+    <script src="https://unpkg.com/leaflet@1.9.1/dist/leaflet.js"
+        integrity="sha256-NDI0K41gVbWqfkkaHj15IzU7PtMoelkzyKp8TOaFQ3s=" crossorigin=""></script>
 
-     {{-- cdn leaflet search --}}
-     <link rel="stylesheet" href="https://labs.easyblog.it/maps/leaflet-search/src/leaflet-search.css">
-     <script src="https://labs.easyblog.it/maps/leaflet-search/src/leaflet-search.js"></script>
-    
+    {{-- cdn leaflet search --}}
+    <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/leaflet-search/3.0.9/leaflet-search.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet-search/3.0.9/leaflet-search.src.js"></script>
+
 
     <style>
         html,
@@ -43,7 +40,6 @@
             max-width: 100%;
             max-height: 100%;
         }
-
     </style>
 
     <style>
@@ -56,18 +52,16 @@
             height: 100%;
             width: 100vw;
         }
-
     </style>
 </head>
 
 <body>
     <div id="map"></div>
     <script>
-       
         var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
             'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             mbUrl =
-            'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+            'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXJpcHJhdGFtYSIsImEiOiJjbGZubmdib3UwbnRxM3Bya3M1NGE4OHRsIn0.oxYqbBbaBwx0dHLguu5gOA';
 
         var satellite = L.tileLayer(mbUrl, {
                 id: 'mapbox/satellite-v9',
@@ -89,7 +83,7 @@
             });
 
         var map = L.map('map', {
-                       
+
             center: [{{ $centrePoint->location }}],
             zoom: 5,
             layers: [streets]
@@ -112,51 +106,55 @@
             L.marker([{{ $item->location }}])
                 .bindPopup(
                     "<div class='my-2'><img src='{{ $item->getImage() }}' class='img-fluid' width='700px'></div>" +
-                    "<div class='my-2'><strong>Nama Space:</strong> <br>{{ $item->name }}</div>" + 
+                    "<div class='my-2'><strong>Nama Space:</strong> <br>{{ $item->name }}</div>" +
                     "<div><a href='{{ route('map.show', $item->slug) }}' class='btn btn-outline-info btn-sm'>Detail Space</a></div>" +
                     "<div class='my-2'></div>"
                 ).addTo(map);
         @endforeach
-    
-        var datas = [    
-        @foreach ($spaces as $key => $value) 
-            {"loc":[{{$value->location }}], "title": '{!! $value->name !!}'},
-        @endforeach            
+
+        var datas = [
+            @foreach ($spaces as $key => $value)
+                {
+                    "loc": [{{ $value->location }}],
+                    "title": '{!! $value->name !!}'
+                },
+            @endforeach
         ];
 
         // pada koding ini kita menambahkan control pencarian data        
         var markersLayer = new L.LayerGroup();
         map.addLayer(markersLayer);
         var controlSearch = new L.Control.Search({
-            position:'topleft',
+            position: 'topleft',
             layer: markersLayer,
             initial: false,
             zoom: 17,
             markerLocation: true
         })
 
-    
-    //menambahkan variabel controlsearch pada addControl
-       map.addControl( controlSearch );
+
+        //menambahkan variabel controlsearch pada addControl
+        map.addControl(controlSearch);
 
         // looping variabel datas utuk menampilkan data space ketika melakukan pencarian data
-        for(i in datas) {
-          
-            var title = datas[i].title,	
-                loc = datas[i].loc,		
-                marker = new L.Marker(new L.latLng(loc), {title: title} );
+        for (i in datas) {
+
+            var title = datas[i].title,
+                loc = datas[i].loc,
+                marker = new L.Marker(new L.latLng(loc), {
+                    title: title
+                });
             markersLayer.addLayer(marker);
 
             // melakukan looping data untuk memunculkan popup dari space yang dipilih
             @foreach ($spaces as $item)
-            L.marker([{{ $item->location }}]
-                )
-                .bindPopup(
-                    "<div class='my-2'><img src='{{ $item->getImage() }}' class='img-fluid' width='700px'></div>" +
-                    "<div class='my-2'><strong>Nama Spot:</strong> <br>{{ $item->name }}</div>" +
-                    "<a href='{{ route('map.show', $item->slug) }}' class='btn btn-outline-info btn-sm'>Detail Spot</a></div>" +
-                    "<div class='my-2'></div>"
-                ).addTo(map);
+                L.marker([{{ $item->location }}])
+                    .bindPopup(
+                        "<div class='my-2'><img src='{{ $item->getImage() }}' class='img-fluid' width='700px'></div>" +
+                        "<div class='my-2'><strong>Nama Spot:</strong> <br>{{ $item->name }}</div>" +
+                        "<a href='{{ route('map.show', $item->slug) }}' class='btn btn-outline-info btn-sm'>Detail Spot</a></div>" +
+                        "<div class='my-2'></div>"
+                    ).addTo(map);
             @endforeach
         }
         L.control.layers(baseLayers, overlays).addTo(map);
